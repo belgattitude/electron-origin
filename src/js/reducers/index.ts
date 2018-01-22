c47571ece98dcd95bddaa486f29fb6e624f9c4f6
belgattitude <belgattitude@gmail.com>
2018-01-22 09:37:05 +0100

    removed console.log

--- src/js/reducers/index.ts
+++ src/js/reducers/index.ts
@@ -7,16 +7,10 @@ const initialState: AppState = {
 };
 
 const rootReducer = (state = initialState, action: any): AppState => {
-    console.log('ROOTREDUCER CALLED', action.type);
-
     switch (action.type) {
       case MEDIA_LOADED:
-          console.log('REDUCER - MEDIA_LOADED', action.payload);
-          const newState = { ...state, media: action.payload };
-          console.log('newState', newState);
           return { ...state, media: action.payload };
       default:
-          console.log('NOTMEDIALOADED');
           return state;
   }
 };
